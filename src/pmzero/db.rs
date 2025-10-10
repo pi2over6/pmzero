2b233dfe2210cd02a76e4fa1aaa75e70cb0bae59
pi2over6 <chequered9104@gmail.com>
2025-10-10 20:35:38 +0900

    feat: pretty print DB
    Signed-off-by: pi2over6 <chequered9104@gmail.com>

--- src/pmzero/db.rs
+++ src/pmzero/db.rs
@@ -1,7 +1,18 @@
-use std::collections::HashMap;
+use std::collections::BTreeMap;
 use std::error::Error;
 
-pub type Members = HashMap<usize, String>;
+pub type Members = BTreeMap<usize, String>;
+
+#[derive(serde::Serialize, serde::Deserialize, Clone)]
+pub struct Game {
+    pub id: usize,
+    pub non_rank_game: bool,
+    pub used_dora_count: Option<u8>,
+    pub recorded_at: String,
+    pub scores: [(usize, i32); 4],
+    pub leftover_score: i32,
+    pub remarks: String,
+}
 
 pub fn load_games() -> Result<Vec<Game>, Box<dyn Error>> {
     let games: Vec<Game> = serde_json::from_str(&std::fs::read_to_string(get_games_path())?)?;
@@ -14,11 +25,11 @@ pub fn load_members() -> Result<Members, Box<dyn Error>> {
 }
 
 pub fn save_games(games: &Vec<Game>) -> Result<(), std::io::Error> {
-    std::fs::write(get_games_path(), serde_json::to_string(games)?)
+    std::fs::write(get_games_path(), serde_json::to_string_pretty(games)?)
 }
 
 pub fn save_members(members: &Members) -> Result<(), std::io::Error> {
-    std::fs::write(get_members_path(), serde_json::to_string(members)?)
+    std::fs::write(get_members_path(), serde_json::to_string_pretty(members)?)
 }
 
 fn get_games_path() -> String {
@@ -27,15 +38,4 @@ fn get_games_path() -> String {
 
 fn get_members_path() -> String {
     std::env::var("MEMBERS_PATH").unwrap_or(String::from("db/members.json"))
-}
-
-#[derive(serde::Serialize, serde::Deserialize, Clone)]
-pub struct Game {
-    pub id: usize,
-    pub non_rank_game: bool,
-    pub used_dora_count: Option<u8>,
-    pub recorded_at: String,
-    pub scores: [(usize, i32); 4],
-    pub leftover_score: i32,
-    pub remarks: String,
 }
